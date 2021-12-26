5b7cf02052c7805f22f09c66e8ce01e5d3b8fa73
Oleg Stelmashchuk <Oleggoper@ukr.net>
2021-12-26 20:58:59 +0200

    Add main js script and jquery

---
+++ views/layouts/default.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link Jquery -->
+    <script src="library/js/jquery.js"></script>
+    <!-- Link main js script -->
+    <script src="public/js/script.js"></script>
+    <title>Blog | <?= $title; ?></title>
+</head>
+<body>
+    <?= $content; ?>
+</body>
+</html>
