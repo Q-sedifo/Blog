df8348ce302bc30b99dab4d5bda049b529e6cacd
Oleg Stelmashchuk <Oleggoper@ukr.net>
2021-12-27 23:43:56 +0200

    Added automated request function for froms
    *Request function in script.js main js file

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,49 @@
 window.onload = () => {
     message.checkMessage();
 }
+
+// Collect data function
+function collectData(form) {
+    const data = {};
+    const postData = form.querySelectorAll('input, textarea');
+
+    postData.forEach(e => {
+        data[e.name] = e.value.trim();
+    });
+
+    return data;
+}
+
+// Ajax requests
+function request(form) {
+    const data = collectData(form);
+
+    const btn = form.querySelectorAll('button');
+    const reload = form.getAttribute('reload');
+
+    $.ajax({
+        url: form.action,
+        type: form.method,
+        data: data,
+        dataType: 'json',
+        beforeSend: () => btn.forEach(e => e.disabled = true),
+        success: (data) => {
+            btn.forEach(e => e.disabled = false);
+            if (data['success']) {
+                if (reload) {
+                    message.saveMessage(data['message'], data['type']);
+                    location.reload();
+                }
+                else {
+                    message.createMessage(data['message'], data['type']);
+                }
+            }
+            else {
+                message.createMessage(data['message'], data['type']);
+            }
+        },
+        error: () => {
+            message.createMessage('Request error', 'error');
+        }
+    });
+}
