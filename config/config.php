bc2b6ebcd1612192073ede706a09e6453d5e0cf4
Oleg Stelmashchuk <Oleggoper@ukr.net>
2022-01-02 22:44:00 +0200

    Added new congig settings
    *config.php -> ImgPath constant fot images
    *gitignore

--- config/config.php
+++ config/config.php
@@ -6,3 +6,11 @@ define('PathPostfix', '.php');
 
 // Define data path constants
 define('AdminData', 'config/data.php');
+
+// Define constants for image path
+define('ImgPath', 'public/images/');
+
+// Acceptable types of file
+return $imgTypes = [
+    'jpeg', 'jpg', 'png', 'gif' 
+];
